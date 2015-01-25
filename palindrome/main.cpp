d13f8932163884aa20b6a0ee042806dde5163abf
John Madison <Madison.John.R@gmail.com>
2015-01-25 11:50:55 -0700

    Palindrome Checker C++
    C++ source code for a palindrome checker example program. This was the
    extra credit assignment for CompSci111 at Colorado Mesa University.

--- palindrome/main.cpp
+++ palindrome/main.cpp
@@ -1,18 +1,19 @@
-//
-//  main.cpp
-//  palindrome
-//
-//  Created by John Madison on 11/15/14.
-//  Copyright (c) 2014 cmu. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[])
+//John Riley Madison - Palidrome Checker - 2014
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    ifstream inf;inf.open("input.txt");
+    ofstream outf;outf.open("output.txt");
+    string input;
+    while  (!inf.eof())
+    {
+        getline(inf,input);
+        if (input == string(input.rbegin(), input.rend()))
+        {outf << left << setw(30) << input << right << " is a palindrome" << endl;}
+        else
+        {outf << left << setw(26) << input << right << " is not a palindrome" << endl;}
+    }
 }
-
